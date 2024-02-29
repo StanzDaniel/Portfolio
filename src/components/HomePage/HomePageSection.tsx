71cfa3597512d71963186afef4ae6997718da2c8
StanzDaniel <stanzioladaniel@gmail.com>
2024-02-29 20:16:16 -0300

    Add: Home Page with Photo, Typewriter and DownArrow

---
+++ src/components/HomePage/HomePageSection.tsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+const HomePageSection = styled.section`
+  background-image: linear-gradient(
+      328deg,
+      rgba(228, 228, 228, 0.04) 0%,
+      rgba(228, 228, 228, 0.04) 30%,
+      rgba(130, 130, 130, 0.04) 30%,
+      rgba(130, 130, 130, 0.04) 49%,
+      rgba(31, 31, 31, 0.04) 49%,
+      rgba(31, 31, 31, 0.04) 100%
+    ),
+    linear-gradient(
+      342deg,
+      rgba(228, 228, 228, 0.04) 0%,
+      rgba(228, 228, 228, 0.04) 20%,
+      rgba(130, 130, 130, 0.04) 20%,
+      rgba(130, 130, 130, 0.04) 60%,
+      rgba(31, 31, 31, 0.04) 60%,
+      rgba(31, 31, 31, 0.04) 100%
+    ),
+    linear-gradient(
+      287deg,
+      rgba(228, 228, 228, 0.04) 0%,
+      rgba(228, 228, 228, 0.04) 29%,
+      rgba(130, 130, 130, 0.04) 29%,
+      rgba(130, 130, 130, 0.04) 48%,
+      rgba(31, 31, 31, 0.04) 48%,
+      rgba(31, 31, 31, 0.04) 100%
+    ),
+    linear-gradient(135deg, rgb(0, 0, 0), rgb(0, 0, 0));
+  min-width: 100vw;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+`;
+
+export default HomePageSection;
---
+++ src/components/HomePage/WelcomeText.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const WelcomeText = styled.div`
+  & span {
+    color: #fff;
+    font-family: "Fira Mono", monospace;
+    font-size: 3rem;
+    font-weight: 500;
+  }
+`;
+
+export default WelcomeText;
